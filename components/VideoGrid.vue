9c60b1aa2d296e712d7cb9cfb29d2ad10dfd93b6
Christian von Uffel <christian.uffel@gmail.com>
2021-08-05 23:16:58 -0500

    enable image scaling

--- components/VideoGrid.vue
+++ components/VideoGrid.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="grid grid-cols-4 gap-4 bg-black text-white px-4">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 bg-black text-white px-4">
         <div v-for="video in videos" :key="video.id" class="mb-6">
             <!-- image preview -->
-            <div class="h-40 bg-gray-500 mb-3"></div>
+            <div class="h-[27vw] md:h-[20vw] lg:h-[15vw] bg-gray-500 mb-3"></div>
             <div class="flex gap-3 pointer-cursor">
                 <!-- channel image -->
                 <div class="rounded-full h-8 w-8 bg-gray-500 overflow-hidden"></div>
