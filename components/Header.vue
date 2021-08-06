357f1823a6410d40065bd3340fff85d09a759e82
Christian von Uffel <christian.uffel@gmail.com>
2021-08-05 23:23:48 -0500

    add search icon

--- components/Header.vue
+++ components/Header.vue
@@ -5,10 +5,13 @@
             <img src="Logo.png" alt="logo" class="h-6">
             
             <!-- search bar -->
-            <form action="" class="hidden md:block">
-                <input type="text" placeholder="Search" class="lg:w-[42vw] bg-black font-light py-1 px-4">
-                <button class="bg-gray-700 py-1 px-6 rounded-r-sm">
-                    O
+            <form action="" class="hidden md:flex items-center">
+                <input type="text" placeholder="Search" class="lg:w-[42vw] bg-black font-light border border-gray-700 rounded-l-sm py-1 px-4">
+                <button class="bg-gray-700 border border-gray-700 py-1.5 px-6 rounded-r-sm">
+                    <!-- search icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
                 </button>
             </form>
 
